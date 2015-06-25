21d1f85041fdac564db0b6d19cac5c31aa5041a4
Alper Hasan <alperhasan@gmail.com>
2015-06-25 17:07:51 +0300

    Extended the purchase method to support all of the braintree options

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -8,22 +8,13 @@ class PurchaseRequest extends AbstractRequest
 
     public function getData()
     {
-        $this->validate('amount');
-        $data['amount'] = $this->getAmount();
+        $data = $this->getParameters();
 
-        $token = $this->getCardReference();
-        if($token) {
-            $data['paymentMethodToken'] = $token;
-        }else {
-            $card = $this->getCardData();
-            if ($card) {
-                $data['creditCard'] = $card;
-            }
-        }
-
-        $data['options'] = array(
-            'submitForSettlement' => true,
-        );
+        unset ($data["card"]);
+        unset ($data["merchantId"]);
+        unset ($data["privateKey"]);
+        unset ($data["publicKey"]);
+        unset ($data["testMode"]);
 
         return $data;
     }
@@ -32,5 +23,223 @@ class PurchaseRequest extends AbstractRequest
     {
         $response = \Braintree_Transaction::sale($data);
         return $this->response = new Response($this, $response);
+    }
+
+
+    public function getDescriptor()
+    {
+        return $this->getParameter('descriptor');
+    }
+
+    public function setDescriptor($value)
+    {
+        return $this->setParameter('descriptor', $value);
+    }
+
+    public function getBilling()
+    {
+        return $this->getParameter('billing');
+    }
+
+    public function setBilling($value)
+    {
+        return $this->setParameter('billing', $value);
+    }
+
+    public function getBillingAddressId()
+    {
+        return $this->getParameter('billingAddressId');
+    }
+
+    public function setBillingAddressId($value)
+    {
+        return $this->setParameter('billingAddressId', $value);
+    }
+
+    public function getChannel()
+    {
+        return $this->getParameter('channel');
+    }
+
+    public function setChannel($value)
+    {
+        return $this->setParameter('channel', $value);
+    }
+
+    public function getCreditCard()
+    {
+        return $this->getParameter('creditCard');
+    }
+
+    public function setCreditCard($value)
+    {
+        return $this->setParameter('creditCard', $value);
+    }
+
+
+    public function getCustomFields()
+    {
+        return $this->getParameter('customFields');
+    }
+
+    public function setCustomFields($value)
+    {
+        return $this->setParameter('customFields', $value);
+    }
+
+    public function getCustomer()
+    {
+        return $this->getParameter('customer');
+    }
+
+    public function setCustomer($value)
+    {
+        return $this->setParameter('customer', $value);
+    }
+
+    public function getCustomerId()
+    {
+        return $this->getParameter('customerId');
+    }
+
+    public function setCustomerId($value)
+    {
+        return $this->setParameter('customerId', $value);
+    }
+
+    public function getDeviceData()
+    {
+        return $this->getParameter('deviceData');
+    }
+
+    public function setDeviceData($value)
+    {
+        return $this->setParameter('deviceData', $value);
+    }
+
+    public function getDeviceSessionId()
+    {
+        return $this->getParameter('deviceSessionId');
+    }
+
+    public function setDeviceSessionId($value)
+    {
+        return $this->setParameter('deviceSessionId', $value);
+    }
+
+    public function getOptions()
+    {
+        return $this->getParameter('deviceSessionId');
+    }
+
+    public function setOptions($value)
+    {
+        return $this->setParameter('deviceSessionId', $value);
+    }
+
+    public function getOrderId()
+    {
+        return $this->getParameter('orderId');
+    }
+
+    public function setOrderId($value)
+    {
+        return $this->setParameter('orderId', $value);
+    }
+
+
+    public function getPaymentMethodNonce()
+    {
+        return $this->getParameter('paymentMethodNonce');
+    }
+
+    public function setPaymentMethodNonce($value)
+    {
+        return $this->setParameter('paymentMethodNonce', $value);
+    }
+
+    public function getPaymentMethodToken()
+    {
+        return $this->getParameter('paymentMethodToken');
+    }
+
+    public function setPaymentMethodToken($value)
+    {
+        return $this->setParameter('paymentMethodToken', $value);
+    }
+
+    public function getPurchaseOrderNumber()
+    {
+        return $this->getParameter('purchaseOrderNumber');
+    }
+
+    public function setPurchaseOrderNumber($value)
+    {
+        return $this->setParameter('purchaseOrderNumber', $value);
+    }
+
+
+    public function getRecurring()
+    {
+        return $this->getParameter('recurring');
+    }
+
+    public function setRecurring($value)
+    {
+        return $this->setParameter('recurring', $value);
+    }
+
+
+    public function getServiceFeeAmount()
+    {
+        return $this->getParameter('serviceFeeAmount');
+    }
+
+    public function setServiceFeeAmount($value)
+    {
+        return $this->setParameter('serviceFeeAmount', $value);
+    }
+
+
+    public function getShipping()
+    {
+        return $this->getParameter('shipping');
+    }
+
+    public function setShipping($value)
+    {
+        return $this->setParameter('shipping', $value);
+    }
+
+    public function getShippingAddressId()
+    {
+        return $this->getParameter('shippingAddressId');
+    }
+
+    public function setShippingAddressId($value)
+    {
+        return $this->setParameter('shippingAddressId', $value);
+    }
+
+
+    public function getTaxAmount()
+    {
+        return $this->getParameter('taxAmount');
+    }
+
+    public function setTaxAmount($value)
+    {
+        return $this->setParameter('taxAmount', $value);
+    }
+
+
+    public function getTaxExempt()
+    {
+        return $this->getParameter('taxExempt');
+    }
+
+    public function setTaxExempt($value)
+    {
+        return $this->setParameter('taxExempt', $value);
     }
 }
