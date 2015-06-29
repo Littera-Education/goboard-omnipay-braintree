d20aa4e8649b50486796211479d1291ab2c27657
Alper Hasan <alperhasan@gmail.com>
2015-06-29 11:13:09 +0300

    Refund and Void refactoring

--- src/Message/RefundRequest.php
+++ src/Message/RefundRequest.php
@@ -8,17 +8,31 @@ class RefundRequest extends AbstractRequest
 
     public function getData()
     {
-        $data = array();
-        $this->validate('amount');
-        $data['amount'] = $this->getAmount();
-        $data['transactionId'] = $this->getTransactionId();
+        $data = $this->getParameters();
+
+        unset ($data["card"]);
+        unset ($data["merchantId"]);
+        unset ($data["privateKey"]);
+        unset ($data["publicKey"]);
+        unset ($data["testMode"]);
+
         return $data;
     }
 
     public function sendData($data)
     {
-        $response = \Braintree_Transaction::refund($data['transactionId'], $data['amount']);
+        $response = \Braintree_Transaction::refund($data['transactionId'], isset($data['amount']) ? $data['amount'] : null);
         return $this->response = new Response($this, $response);
     }
 
+
+    public function getTransactionId()
+    {
+        return $this->getParameter('transactionId');
+    }
+
+    public function setDescriptor($value)
+    {
+        return $this->setTransactionId('transactionId', $value);
+    }
 }
